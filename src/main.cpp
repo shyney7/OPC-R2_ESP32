aeb996c0c1c211b9cf025617faf16f690aeecf6e
Marcel <ligimixi@gmail.com>
2023-05-25 13:06:24 +0200

    added BME280(I2C)

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,14 @@
 #include <SPI.h>
 #include <esp_task_wdt.h>
 #include "opcr2.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+//Adafruit_BME280 bme I2C
+Adafruit_BME280 bme;
+#define SEALEVELPRESSURE_HPA (1013.25)
+//print values prototype
+void printValues(void);
 
 void setup() {
     // put your setup code here, to run once:
@@ -16,18 +24,33 @@ void setup() {
 
     //Start serial port
     opSerial.begin(BaudRate);
+    while (!opSerial) {
+        delay(100);
+    }
 
     PrintFirmwareVer(opSerial);
 
     SPI.begin();
 
     esp_task_wdt_reset();
-    InitDevice();
+    InitDevice(); // init OPC
     esp_task_wdt_reset();
 
-    PrintDataLabels(opSerial);
+    PrintDataLabels(opSerial); //OPC data labels
     currentTime = millis();
     cloopTime = currentTime;
+    //bme280
+    unsigned status;
+    status = bme.begin();
+    if (!status) {
+        opSerial.println("Could not find a valid BME280 sensor, check wiring!");
+        opSerial.print("SensorID was: 0x"); opSerial.println(bme.sensorID(), 16);
+        opSerial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+        opSerial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+        opSerial.print("        ID of 0x60 represents a BME 280.\n");
+        opSerial.print("        ID of 0x61 represents a BME 680.\n");
+        while (1) delay(10);
+    }
 }
 
 void loop() {
@@ -70,7 +93,28 @@ void loop() {
         opSerial.print(millis());
         PrintData(opSerial); //Print data to serial
         esp_task_wdt_reset(); //Reset watchdog timer
-
+        printValues(); //bme280
+        esp_task_wdt_reset();
     }
     
+}
+
+void printValues(void) {
+    opSerial.print("Temperature = ");
+    opSerial.print(bme.readTemperature());
+    opSerial.println(" °C");
+
+    opSerial.print("Pressure = ");
+    opSerial.print(bme.readPressure() / 100.0F);
+    opSerial.println(" hPa");
+
+    opSerial.print("Approx. Altitude = ");
+    opSerial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    opSerial.println(" m");
+
+    opSerial.print("Humidity = ");
+    opSerial.print(bme.readHumidity());
+    opSerial.println(" %");
+
+    opSerial.println();
 }
